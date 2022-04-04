f1abecb66229becb808209d89a38d12639602ea7
CLAUDIO <cl.louzeiro@gmail.com>
2022-04-03 21:41:25 -0300

    [UPDATE] criando rotas da API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +15,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'namespace' => 'App\Http\Controllers'
+],function(){
+    Route::group([
+        'prefix' => 'moradores',
+    ],function(){
+        Route::get('/{id_morador?}', 'MoradoresController@index');
+    });
+
+    Route::group([
+        'prefix' => 'vaga-morador',
+    ],function(){
+        Route::get('/indica-vaga', 'VagasMoradorController@indicaVaga');
+    });
 });
